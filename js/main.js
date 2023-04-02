55a9652a88bcbb7652e84118c7adf71aaa9dc7a2
jakeblacklds <jakeblacklds@gmail.com>
2023-04-02 17:52:46 -0600

    Se hizo una tienda de frutas con el uso de arrays y funciones

--- js/main.js
+++ js/main.js
@@ -1,89 +1,162 @@
 
-/* Programa para advinar un numero aleatorio entre 1 y 10! */
 
-//Esta es linea de codigo la busque, ya que no sabia como generar un numero aleatorio
+let frutas = [
+    {nombre: "Manzana", precio: 20},
+    {nombre: "Banana", precio: 30},
+    {nombre: "Naranja", precio: 15},
+    {nombre: "Fresa", precio: 65},
+    {nombre: "Pera", precio: 25},
+    {nombre: "Melon", precio: 50},
+    {nombre: "Sandia", precio: 40},
+    {nombre: "Kiwi", precio: 35},
+    {nombre: "Uva", precio: 45},
+    {nombre: "Mango", precio: 55},
+    {nombre: "Papaya", precio: 60},
+    {nombre: "Cereza", precio: 70},
+    {nombre: "Ciruela", precio: 75},
+    {nombre: "Pitaya", precio: 80},
+    {nombre: "Mandarina", precio: 85},
+]
 
-let puntajeTotal = 0
-menuPrincipal()
+// Crear un array vacío para el carrito
+let carrito = []
+
+// Mostrar el menú de opciones al usuario
+menu()
+
+function menu() {
+let opcion = prompt("Bienvenido a la tienda de frutas. Elige una opción:\n" +
+    "1. Ver el nombre y el precio de cada fruta\n" +
+    "2. Añadir una fruta al carrito\n" +
+    "3. Total del carrito y Pagar\n" +
+    "4. Salir")
+
+// Ejecutar la opción elegida por el usuario
+
+switch (opcion) {
+
+  case "1": // Ver el nombre y el precio de cada fruta
+    let mensaje = "" // Crear un mensaje vacío
+    frutas.forEach(function(fruta) { // Recorrer el array de frutas
+      mensaje += fruta.nombre + ": " + fruta.precio + "$\n"; // Añadir el nombre y el precio de cada fruta al mensaje
+    })
+    alert(mensaje) // Mostrar el mensaje con las frutas y sus precios
+    menu()
+    break   
 
 
-function menuPrincipal () {
-    let menu = parseInt(prompt("Bienvenido al juego de adivinar el número! \n 1. Jugar \n 2. Puntaje Total\n 3. Salir"))
-    switch (menu) {
-        case 1:
-            adivinarNumero()
-            break
-        case 2:
-            alert("El puntaje total es de " + puntajeTotal)
-            menuPrincipal()
-            break2
-        case 3:
-            alert("Gracias por jugar!")
-            break
-        default:
-            alert("Opción inválida")
-            menuPrincipal()
-    }
+
+    case "2": // Añadir una fruta al carrito
+    BuscayAñade()
+    break
+
+    case "3": // Ver el total del Carrito y Pagar
+    vercarrito() 
+    pagar()
+    break
+
+    case "4": // Salir
+    alert("Gracias por su visita"); // Mostrar un mensaje de despedida
+    break
+
+    default: 
+    alert("Opción no válida"); 
+    menu() 
 }
 
-function adivinarNumero() {
-let numeroAleatorio = Math.floor(Math.random() * 10) + 1
-//Aqui empieza el bucle para que el usuario tenga 5 intentos para adivinar el numero
-for (let i = 0; i < 5; i++) {
-    alert("Tienes 5 intentos para adivinar el número" + "\n" + "             Intento " + (i + 1) + " de 5")
-    // Se le pide al usuario que ingrese un numero
-    let numeroIngresado = parseInt(prompt("Ingrese un número entre 1 y 10"))
-        
-    // Aqui se compara el numero ingresado con el numero aleatorio,
-        if (numeroIngresado == numeroAleatorio) {
-            // se hizo una funcion para calcular el puntaje de acuerdo a los intentos
-            
-            alert("Ganaste! Obtuviste  " + puntaje(i) + "  puntos")
-            puntajeTotal += puntaje(i)
-            // Se le pregunta al usuario si desea seguir jugando
-            jugarOtraVez()
-            break
+}
+
+function BuscayAñade() {
+    let nombre = prompt("Introduce el nombre de la fruta que quieres buscar"); // Pedir el nombre de la fruta que se quiere buscar al usuario
+    let resultado = frutas.find(function(fruta) { // Usar el método find para obtener la primera fruta que coincida con el nombre buscado
+        return fruta.nombre === nombre; // Devolve2r verdadero si la fruta actual tiene el mismo nombre que el buscado
+    })
+    if (resultado) { // Si se ha encontrado una fruta
+        alert("El precio de la " + nombre + " es " + resultado.precio + " $"); // Mostrar el precio de la fruta
+        alert("¿Desea añadir la fruta al carrito?") // Preguntar si se quiere añadir la fruta al carrito
+        let respuesta = prompt("1. Si \n 2. No") // Pedir la respuesta al usuario
+        if (respuesta == "1") { // 
+            carrito.push(resultado) // Añadir la fruta al carrito
+            alert("Se ha añadido la fruta " + nombre + " al carrito"); // Mostrar un mensaje de confirmación
+            menu()
         } else {
-            if (numeroIngresado > numeroAleatorio) {
-                alert("El número es menor")
-            } else {   
-                alert("El número es mayor")
-            }
-                } 
-    // Si el usuario no adivina el numero en los 5 intentos, se le muestra el numero aleatorio
-    if (i == 4) {
-        alert("Perdiste! El número era " + numeroAleatorio)
-        jugarOtraVez()
+            alert("No se ha añadido la fruta al carrito"); // Mostrar un mensaje de que no se ha añadido la fruta al carrito
+        }
+        
+
+    } else { // Si no se ha encontrado ninguna fruta
+        alert("Ahorita no tenemos " + nombre + " intente de nuevo" ); // Mostrar un mensaje de que no se ha encontrado la fruta
+        BuscayAñade() 
     }
+
+}
+
+function vercarrito() {
+  let mensaje = "" 
+  let total = sumarProductos(carrito)
+  carrito.forEach(function(fruta) { // Recorrer el array del carrito
     
-
-} 
+    mensaje += fruta.nombre + ":  " + fruta.precio + "$\n"  // Añadir el nombre y el precio de cada fruta al mensaje
+    })
+  alert(mensaje + "Total de   " + total + "$") // Mostrar el mensaje con las frutas y sus precios
 
 }
 
+function quitar() {
+    let nombre = prompt("Introduce el nombre de la fruta que quieres quitar"); // Pedir el nombre de la fruta que se quiere buscar al usuario
+    let resultado = carrito.find(function(fruta) { // Usar el método find para obtener la primera fruta que coincida con el nombre buscado
+        return fruta.nombre === nombre; // Devolver verdadero si la fruta actual tiene el mismo nombre que el buscado
+    })
+    if (resultado) { // Si se ha encontrado una fruta
+        alert("¿Desea quitar la fruta del carrito?") // Preguntar si se quiere añadir la fruta al carrito
+        let respuesta = prompt("1. Si \n 2. No") // Pedir la respuesta al usuario
+        if (respuesta === "1") { // 
+            carrito.pop(resultado) //   Quitar la fruta del carrito
+            alert("Se ha quitado la fruta " + nombre + " del carrito"); // Mostrar un mensaje de confirmación
+            menu()
+        } else { // Si la respuesta es no
+            quitar() 
+        }
+    } else { // Si no se ha encontrado ninguna fruta
+        alert("La fruta " + nombre + " No esta en el carrito" ); // Mostrar un mensaje de que no se ha encontrado la fruta
+        quitar()
+    }
 
-//Esta funcion calcula el puntaje del usuario, se le resta 200 por cada intento.Por lo tanto, si falla en todos los intentos obtendra 0 puntos
-function puntaje (intentos)
-{
-    let puntaje = 1000 - (intentos * 200)
-    return puntaje
 }
 
-function jugarOtraVez () {
-    let otravez = parseInt(prompt("¿Desea jugar otra vez? \n 1. Si \n 2. Puntaje Total \n 3. Menu Principal"))
-    switch (otravez) {
-        case 1:
-            adivinarNumero()
-            break
-        case 2:
-            alert ("El puntaje total es de " + puntajeTotal)
-            jugarOtraVez()
-            break
-        case 3:
-            menuPrincipal()
-            break
-        default:
-            alert("Opción inválida")
-            jugarOtraVez()
+function pagar() {
+    alert("Quiere pagar?") // Preguntar si se quiere pagar
+    let respuesta = prompt("1. Si \n 2. No") // Pedir la respuesta al usuario
+    if (respuesta === "1") { //
+        let pago = prompt("¿Con cuánto va a pagar?") // Pedir el pago al usuario
+        let total = 0 // Crear una variable para el total
+        carrito.forEach(function(fruta) { // Recorrer el array del carrito
+            total += fruta.precio // Sumar el precio de cada fruta al total
+        })
+        if (pago >= total) { // Si el pago es mayor o igual que el total
+            alert("Su cambio es de " + (pago - total) + "$"); // Mostrar el cambio
+            alert("Gracias por su compra"); // Mostrar un mensaje de despedida
+            
+        } else { // Si el pago es menor que el total
+            alert("No le alcanza, quite cosas del carrito"); // Mostrar un mensaje de que el pago es insuficiente
+            quitar()
+            //Despues de quitar algo del carrito, volver a preguntar si quiere pagar
+            pagar()
+            
+        }
+    } else 
+     { // Si la respuesta es no
+        menu()
+        }
+        
 }
+
+function sumarProductos(productos) {
+    {
+        let total = 0
+        for (let i = 0; i < productos.length; i++) {
+        total += productos[i].precio
+        }
+        return total
+        }
 }
