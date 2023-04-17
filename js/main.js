3fcf6bad56fa393c1e575f3a6de45cd415a3d74b
jakeblacklds <jakeblacklds@gmail.com>
2023-04-16 18:37:43 -0600

    Se hizo un pequeño e-commerce, se pueden clasificar los productos por precio y se puede agregar y eliminar del carrito

--- js/main.js
+++ js/main.js
@@ -1,162 +1,183 @@
+// Lista de productos de productos gamer con ID, categoría, nombre, precio e imagen
+let productos = [
+  {
+    id: 1,
+    categoria: "mouse",
+    nombre: "mouse Logitech G502",
+    descripcion: "Mouse gamer con 6 botones y sensor óptico",
+    precio: 1000,
+    imagen: "https://www.pchmayoreo.com/pub/media/catalog/product/0/9/097855144430-L.jpg",
+  },
+  {
+    id: 2,
+    categoria: "mouse",
+    nombre: "mouse Razer Deathadder",
+    descripcion: "Mouse Razer con luces RGB ",
+    precio: 1530,
+    imagen: "https://m.media-amazon.com/images/I/51QHGpyTqFL._AC_SS450_.jpg",
+  },
+  {
+    id: 3,
+    categoria: "teclado",
+    nombre: "teclado razer blackwidow",
+    descripcion: "Teclado mecánico con luces RGB",
+    precio: 3400,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_638619-MLA45374821312_032021-O.jpg",
+  },
+  {
+    id: 4,
+    categoria: "teclado",
+    nombre: "teclado logitech G512",
+    descripcion: "Teclado mecánico con luces RGB",
+    precio: 1400,
+    imagen: "https://ddtech.mx/assets/uploads/8d10737356137a6395b34619741ddbb6.png",
+  },
+  {
+    id: 5,
+    categoria: "monitor",
+    nombre: "monitor gamer AOC 24",
+    descripcion: "Monitor  de 24 pulgadas  Full HD",
+    precio: 10000,
+    imagen: "https://http2.mlstatic.com/D_NQ_NP_758769-MLA47598712227_092021-O.jpg",
+  },
+  {
+    id: 6,
+    categoria: "monitor",
+    nombre: "monitor Gamer AOC 27",
+    descripcion: "Monitor  de 27 pulgadas con resolución 4K",
+    precio: 20000,
+    imagen: "https://i1.wp.com/garrcorpcomputer.com/wp-content/uploads/2021/09/monitor-gamer-8.png?fit=561%2C493&ssl=1",
+  },
+  {
+    id: 7,
+    categoria: "auriculares",
+    nombre: "auriculares hyperX cloud",
+    descripcion: "Auriculares gamer con micrófono",
+    precio: 5000,
+    imagen: "https://webonline.macstore.mx/img/sku/ACCHYP002_FZ.jpg",
+  },
+  {
+    id: 8,
+    categoria: "auriculares",
+    nombre: "auriculares hyperX cloud II",
+    descripcion: "Auriculares gamer con micrófono",
+    precio: 7000,
+    imagen: "https://ddtech.mx/assets/uploads/0629982c1acd2784bdee264d96384a4f.jpg",
+  },
+  ]
 
+  let contenedorProductos = document.getElementById("productos")
 
-let frutas = [
-    {nombre: "Manzana", precio: 20},
-    {nombre: "Banana", precio: 30},
-    {nombre: "Naranja", precio: 15},
-    {nombre: "Fresa", precio: 65},
-    {nombre: "Pera", precio: 25},
-    {nombre: "Melon", precio: 50},
-    {nombre: "Sandia", precio: 40},
-    {nombre: "Kiwi", precio: 35},
-    {nombre: "Uva", precio: 45},
-    {nombre: "Mango", precio: 55},
-    {nombre: "Papaya", precio: 60},
-    {nombre: "Cereza", precio: 70},
-    {nombre: "Ciruela", precio: 75},
-    {nombre: "Pitaya", precio: 80},
-    {nombre: "Mandarina", precio: 85},
-]
+ 
 
-// Crear un array vacío para el carrito
-let carrito = []
+  let buscador = document.getElementById("buscador")
+  buscador.addEventListener("input", filtroNombre)
 
-// Mostrar el menú de opciones al usuario
-menu()
+  let menorPrecio = document.getElementById("menor-precio")
+  let filtradoPorMenorPrecio = false;
+  menorPrecio.addEventListener("click", filtroMenorPrecio)
 
-function menu() {
-let opcion = prompt("Bienvenido a la tienda de frutas. Elige una opción:\n" +
-    "1. Ver el nombre y el precio de cada fruta\n" +
-    "2. Añadir una fruta al carrito\n" +
-    "3. Total del carrito y Pagar\n" +
-    "4. Salir")
+  let mayorPrecio = document.getElementById("mayor-precio")
+  let filtradoPorMayorPrecio = false;
+  mayorPrecio.addEventListener("click", filtroMayorPrecio)
 
-// Ejecutar la opción elegida por el usuario
+  mostrarProductos( productos )
+  mostrarCarrito()
 
-switch (opcion) {
+  // Función para filtrar los productos por nombre usando el buscador poniendo letra por letra
+  function filtroNombre() {
+    let listaFiltrada = productos.filter(producto => producto.nombre.includes(buscador.value))  
+    mostrarProductos(listaFiltrada)
+  }
 
-  case "1": // Ver el nombre y el precio de cada fruta
-    let mensaje = "" // Crear un mensaje vacío
-    frutas.forEach(function(fruta) { // Recorrer el array de frutas
-      mensaje += fruta.nombre + ": " + fruta.precio + "$\n"; // Añadir el nombre y el precio de cada fruta al mensaje
-    })
-    alert(mensaje) // Mostrar el mensaje con las frutas y sus precios
-    menu()
-    break   
-
-
-
-    case "2": // Añadir una fruta al carrito
-    BuscayAñade()
-    break
-
-    case "3": // Ver el total del Carrito y Pagar
-    vercarrito() 
-    pagar()
-    break
-
-    case "4": // Salir
-    alert("Gracias por su visita"); // Mostrar un mensaje de despedida
-    break
-
-    default: 
-    alert("Opción no válida"); 
-    menu() 
-}
-
-}
-
-function BuscayAñade() {
-    let nombre = prompt("Introduce el nombre de la fruta que quieres buscar"); // Pedir el nombre de la fruta que se quiere buscar al usuario
-    let resultado = frutas.find(function(fruta) { // Usar el método find para obtener la primera fruta que coincida con el nombre buscado
-        return fruta.nombre === nombre; // Devolve2r verdadero si la fruta actual tiene el mismo nombre que el buscado
-    })
-    if (resultado) { // Si se ha encontrado una fruta
-        alert("El precio de la " + nombre + " es " + resultado.precio + " $"); // Mostrar el precio de la fruta
-        alert("¿Desea añadir la fruta al carrito?") // Preguntar si se quiere añadir la fruta al carrito
-        let respuesta = prompt("1. Si \n 2. No") // Pedir la respuesta al usuario
-        if (respuesta == "1") { // 
-            carrito.push(resultado) // Añadir la fruta al carrito
-            alert("Se ha añadido la fruta " + nombre + " al carrito"); // Mostrar un mensaje de confirmación
-            menu()
-        } else {
-            alert("No se ha añadido la fruta al carrito"); // Mostrar un mensaje de que no se ha añadido la fruta al carrito
-        }
-        
-
-    } else { // Si no se ha encontrado ninguna fruta
-        alert("Ahorita no tenemos " + nombre + " intente de nuevo" ); // Mostrar un mensaje de que no se ha encontrado la fruta
-        BuscayAñade() 
+  // Función para filtrar los productos por precio de menor a mayor y se ordene otra vez al hacer click
+  function filtroMenorPrecio() {
+        // se le cambia el valor a la variable para que se active el filtro cuando se haga click en el checkbox
+    filtradoPorMenorPrecio = !filtradoPorMenorPrecio; 
+    // se uso la Funcion de Orden Superior sort() para reordenar los productos por precio
+    if (filtradoPorMenorPrecio) {
+      // si el filtro está activo, se ordena de menor a mayor precio
+      productos.sort((a, b) => a.precio - b.precio);
+    } else {
+      // si el filtro está desactivado, se vuelve a mostrar la lista original
+      productos.sort((a, b) => a.id - b.id);
     }
+    mostrarProductos(productos);
+  }
 
+  // Función para filtrar los productos por precio de mayor a menor y se ordene otra vez al hacer click
+  function filtroMayorPrecio() {
+    filtradoPorMayorPrecio = !filtradoPorMayorPrecio; 
+    if (filtradoPorMayorPrecio) {
+      productos.sort((a, b) => b.precio - a.precio);
+    } else {
+      productos.sort((a, b) => a.id - b.id);
+    }
+    mostrarProductos(productos);
+
+  }
+
+// Función para mostrar los productos en la página usando innerHTML
+function mostrarProductos(listaProductos ) {
+  contenedorProductos.innerHTML = ""
+  listaProductos.forEach((producto) => {
+    let cajaProducto = document.createElement("div")
+    cajaProducto.className = "caja-producto"
+    cajaProducto.innerHTML = `
+      <h3>${producto.nombre}</h3>
+      <p>${producto.descripcion}</p>
+      <p>Precio: $${producto.precio}</p>
+      <img class= "imagen" style="width: 200px; height: 200px;" src="${producto.imagen}" >
+      <button onclick="agregarAlCarrito(${producto.id})">Agregar al carrito</button>
+    `
+
+    contenedorProductos.appendChild(cajaProducto)
+
+
+  })
 }
 
-function vercarrito() {
-  let mensaje = "" 
-  let total = sumarProductos(carrito)
-  carrito.forEach(function(fruta) { // Recorrer el array del carrito
+// Función para agregar productos al carrito y se guarden en el localStorage
+function agregarAlCarrito(id) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+  let producto = productos.find((producto) => producto.id === id)
+  carrito.push(producto)
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+  mostrarCarrito()
+}
+
+// Función para mostrar los productos que se agregaron al carrito
+function mostrarCarrito() {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+  let contenedorCarrito = document.getElementById("carrito")
+  contenedorCarrito.innerHTML = ""
+  carrito.forEach((producto) => {
+    let cajaProducto = document.createElement("div")
+    cajaProducto.className = "caja-producto"
+    cajaProducto.innerHTML = `
+      <h4>${producto.nombre}</h4>
+      <p>${producto.descripcion}</p>
+      <p>Precio: $${producto.precio}</p>
+      <img class= "imagen-carrito" style="width: 100px; height: 100px;" src="${producto.imagen}" >
+      <button onclick="eliminarDelCarrito(${producto.id})">Eliminar del carrito</button>
+    `
+    //Eliminamos el producto de la página
     
-    mensaje += fruta.nombre + ":  " + fruta.precio + "$\n"  // Añadir el nombre y el precio de cada fruta al mensaje
-    })
-  alert(mensaje + "Total de   " + total + "$") // Mostrar el mensaje con las frutas y sus precios
-
+    contenedorCarrito.appendChild(cajaProducto)
+ 
+  })
 }
 
-function quitar() {
-    let nombre = prompt("Introduce el nombre de la fruta que quieres quitar"); // Pedir el nombre de la fruta que se quiere buscar al usuario
-    let resultado = carrito.find(function(fruta) { // Usar el método find para obtener la primera fruta que coincida con el nombre buscado
-        return fruta.nombre === nombre; // Devolver verdadero si la fruta actual tiene el mismo nombre que el buscado
-    })
-    if (resultado) { // Si se ha encontrado una fruta
-        alert("¿Desea quitar la fruta del carrito?") // Preguntar si se quiere añadir la fruta al carrito
-        let respuesta = prompt("1. Si \n 2. No") // Pedir la respuesta al usuario
-        if (respuesta === "1") { // 
-            carrito.pop(resultado) //   Quitar la fruta del carrito
-            alert("Se ha quitado la fruta " + nombre + " del carrito"); // Mostrar un mensaje de confirmación
-            menu()
-        } else { // Si la respuesta es no
-            quitar() 
-        }
-    } else { // Si no se ha encontrado ninguna fruta
-        alert("La fruta " + nombre + " No esta en el carrito" ); // Mostrar un mensaje de que no se ha encontrado la fruta
-        quitar()
-    }
-
+// Función para eliminar productos del carrito y del local storage
+function eliminarDelCarrito(id) {
+  let carrito = JSON.parse(localStorage.getItem("carrito")) || []
+  carrito = carrito.filter((producto) => producto.id !== id)
+  localStorage.setItem("carrito", JSON.stringify(carrito))
+  mostrarCarrito()
 }
 
-function pagar() {
-    alert("Quiere pagar?") // Preguntar si se quiere pagar
-    let respuesta = prompt("1. Si \n 2. No") // Pedir la respuesta al usuario
-    if (respuesta === "1") { //
-        let pago = prompt("¿Con cuánto va a pagar?") // Pedir el pago al usuario
-        let total = 0 // Crear una variable para el total
-        carrito.forEach(function(fruta) { // Recorrer el array del carrito
-            total += fruta.precio // Sumar el precio de cada fruta al total
-        })
-        if (pago >= total) { // Si el pago es mayor o igual que el total
-            alert("Su cambio es de " + (pago - total) + "$"); // Mostrar el cambio
-            alert("Gracias por su compra"); // Mostrar un mensaje de despedida
-            
-        } else { // Si el pago es menor que el total
-            alert("No le alcanza, quite cosas del carrito"); // Mostrar un mensaje de que el pago es insuficiente
-            quitar()
-            //Despues de quitar algo del carrito, volver a preguntar si quiere pagar
-            pagar()
-            
-        }
-    } else 
-     { // Si la respuesta es no
-        menu()
-        }
-        
-}
 
-function sumarProductos(productos) {
-    {
-        let total = 0
-        for (let i = 0; i < productos.length; i++) {
-        total += productos[i].precio
-        }
-        return total
-        }
-}
+
+
+
+
